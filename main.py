25d4f034fad17a177a1cce7aef60f9265fadfa96
Mark Bobkov <mark.bobkov.13@gmail.com>
2020-09-29 22:49:38 +0300

    Added web form to edit keywords

--- main.py
+++ main.py
@@ -6,15 +6,29 @@ import threading
 import config
 
 
+def load_keywords():
+  with open(config.keywords_file, 'r') as file:
+    data = file.read()
+  return data.lower().strip().split(', ')
+
+
+def save_keywords(keywords):
+  with open(config.keywords_file, 'w') as file:
+    file.write(config.joiner.join(keywords).lower().strip())
+  return
+
+
+keywords = load_keywords() # list with keywords to search in messages
+
 client = TelegramClient(config.api_sess_name,
                         config.api_id,
                         config.api_hash)
 
-ignore_chats = [] # chats to ignore
-keywords = ["слово", "ключ", "таргет"] # all words must be in lowercase
 
 @client.on(events.NewMessage)
 async def my_event_handler(event):
+
+  global keywords
 
   chat_id = event.chat_id
   sender_id = event.sender_id
@@ -22,7 +36,7 @@ async def my_event_handler(event):
   text = event.message.message.lower() # get message text in lowercase
   message_id = event.message.id
 
-  if chat_id not in ignore_chats: # check if we searching keywords in this chat
+  if chat_id not in config.ignore_chats: # check if we searching keywords in this chat
     if text and any(word in text for word in keywords): # check if message text matches search keywords
 
       target_chat = await client.get_entity(config.target_chat_id) # Group to send messages matched
@@ -47,8 +61,31 @@ WEB
 routes = web.RouteTableDef()
 
 @routes.get('/')
-async def hello(request):
-    return web.Response(text="Hello, world")
+async def index(request):
+
+  global keywords
+
+  with open('index.html', 'r') as file:
+    html = file.read()
+  html = html.replace("{{keywords}}", config.joiner.join(keywords))
+
+  return web.Response(text=html, content_type='text/html')
+
+
+@routes.post('/save')
+async def save(request):
+
+  global keywords
+
+  form = await request.post()
+
+  new_keywords = form.get('keywords', "")
+  new_keywords = new_keywords.split(config.joiner)
+
+  save_keywords(new_keywords)
+  keywords = new_keywords
+  
+  raise web.HTTPFound('/')
 
 
 app = web.Application()
@@ -61,7 +98,7 @@ loop = asyncio.get_event_loop()
 async def start() -> None:
     await client.start()
     await runner.setup()
-    site = web.TCPSite(runner, '0.0.0.0', 5000)
+    site = web.TCPSite(runner, config.host, config.port)
     await site.start()
 
 
@@ -72,13 +109,16 @@ async def stop() -> None:
 
 loop.run_until_complete(start())
 print("========== Initialization complete ==========")
-print(f"Listening at http://")
-print(f"Public URL prefix is")
+print("========== Initialization complete ==========")
+print(f"Listening at http://{config.host}:{config.port}")
+print(f"Public URL prefix is {config.public_url}")
 print("(Press CTRL+C to quit)")
+
 try:
     loop.run_forever()
 except KeyboardInterrupt:
     loop.run_until_complete(stop())
-except Exception:
+except Exception as e:
     print("Fatal error in event loop")
+    print(str(e))
     raise
